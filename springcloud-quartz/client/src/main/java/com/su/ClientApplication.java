c01f2a7ea47ccd5f2c888fa4277ca5c4bba3ab46
suwliang <suwliang_3@163.com>
2020-08-10 18:39:06 +0800

    第一次修改

--- springcloud-quartz/client/src/main/java/com/su/ClientApplication.java
+++ springcloud-quartz/client/src/main/java/com/su/ClientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//启动
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients
 public class ClientApplication {
